763e9d0a931b27a08352262d7d25d40fbd757810
Harshada Keste <kesteharshada87@gmail.com>
2026-01-14 13:38:21 +0530

    c# variables and constants

---
+++ Day01/demo02.cs
@@ -0,0 +1,13 @@
+int num = 8;
+Console.WriteLine(num);
+
+string name = "Harshada";
+Console.WriteLine("My name is: "+ name);
+
+double myDoubleNum = 5.99D;
+char myLetter = 'D';
+bool myBool = true;
+
+//const int a = 8;
+//a = 10;
+//Console.WriteLine(a);-->error
