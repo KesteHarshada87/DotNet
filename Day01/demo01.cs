0de01091c836150b6fa68cea4b04867e7e678a94
Harshada Keste <kesteharshada87@gmail.com>
2026-01-14 13:22:17 +0530

    C sharp basic code syntax

---
+++ Day01/demo01.cs
@@ -0,0 +1,15 @@
+using System;//using System means that we can use classes from the System namespace.
+
+namespace HelloWorld//namespace is used to organize your code, and it is a container for classes and other namespaces.
+{
+
+    //class is a container for data and methods, which brings functionality to your program. Every line of code that runs in
+    //  C# must be inside a class. In our example, we named the class Program.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");    
+        }
+    }
+}
